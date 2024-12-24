a9639fa35f8a2153a97792da284efb4bc85e11af
RanjitXtha <132259585+RanjitXtha@users.noreply.github.com>
2024-12-24 22:05:15 +0545

    changes made

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../Components/Header'
 import Hero from '../Components/Hero'
 import Services from '../Components/Services'
+import Category from '../Components/Category'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Header />
         <Hero />
         <Services />
+        <Category />
     </div>
   )
 }
